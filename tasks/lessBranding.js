a199e070c7461fd7a978f7b68d0cb6416cad4e2e
Mark Terry <mark.terry@holidayextras.com>
2015-02-03 11:12:20 +0000

    better use of brand

--- tasks/lessBranding.js
+++ tasks/lessBranding.js
@@ -17,9 +17,9 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  var _findLess = function(options, base, name){
+  var _findLess = function(brands, base, name){
     var found = [];
-    options.brands.every(function(brand){
+    brands.every(function(brand){
       var lessPath = path.join(base, 'assets', 'style', brand, 'main.less');
       if(fs.existsSync(lessPath)){
         grunt.log.debug('Found:', lessPath);
@@ -38,26 +38,23 @@ module.exports = function(grunt) {
 
 
   grunt.registerMultiTask('lessBranding', 'Process branded LESS', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      brands: []
-    });
-
     var pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
-    options.brands.push('');  //run default brand too
-    grunt.log.debug('Using brands: ', options.brands.join("\n  "));
+    var brands = [];
+    if(options.brand) brands.push(options.brand);
+    brands.push('');  //run default brand too
+    grunt.log.debug('Using brands: ', brands.join("\n  "));
 
     var moduleImports = [];
 
     Object.keys(pkg.dependencies).forEach(function(moduleName){
       var base = path.join('node_modules', moduleName);
       grunt.log.debug('Searching in:', base);
-      moduleImports = moduleImports.concat(_findLess(options, base, moduleName));
+      moduleImports = moduleImports.concat(_findLess(brands, base, moduleName));
     });
 
     grunt.log.debug('Adding app specific LESS');
-    moduleImports = moduleImports.concat(_findLess(options, ''));
+    moduleImports = moduleImports.concat(_findLess(brands, ''));
 
     var lessOptions = {
       //FIXME does nothing in render's grunt-contrib-less strictImports: true,
@@ -70,7 +67,8 @@ module.exports = function(grunt) {
     less.render(moduleImports.join("\n"), lessOptions).then(function(output){
       grunt.log.debug('CSS output:');
       grunt.log.debug(output.css);
-      var out = path.join('www', 'main.css');
+      var brandFile = options.brand || 'main';
+      var out = path.join('www', brandFile + '.css');
       grunt.file.write(out, output.css);
       grunt.log.writeln('File "' + out + '" created.');
     },
